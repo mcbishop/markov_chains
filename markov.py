29476ed8d12186dbe3795b6204c3076443367995
Hackbright Student <info@hackbrightacademy.com>
2016-01-12 17:33:11 -0800

    Updated make change to accept variable engram length.

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 from random import choice
 from sys import argv
 
+ngram_length = 3
+
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
@@ -14,7 +16,7 @@ def open_and_read_file(file_path):
         return text_corpus
 
 
-def make_chains(text_string):
+def make_chains(text_string,ngram_length):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -26,23 +28,25 @@ def make_chains(text_string):
         >>> make_chains("hi there mary hi there juanita")
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
+    n = ngram_length
 
     chains = {}
 
     text_tokens = text_string.split()
 
     for i in range(len(text_tokens)):
-        if i < (len(text_tokens) - 2):
-            ngram = (text_tokens[i], text_tokens[i+1])
+        if i < (len(text_tokens) - n): # stop at ngram length before end 
+            ngram = tuple(text_tokens[i:i+n]) #creates ngram
             if ngram not in chains:
                 chains[ngram] = []
-            chains[ngram].append(text_tokens[i+2])
+            chains[ngram].append(text_tokens[i+n]) #index is based on ngram length
 
     return chains
 
 
-def make_text(chains):
+def make_text(chains,ngram_length):
     """Takes dictionary of markov chains; returns random text."""
+    n = ngram_length
 
     text = ""
 
@@ -50,9 +54,9 @@ def make_text(chains):
     ngram = first_ngram
     counter = 0
     while ((ngram in chains) and counter < 500):
-        second_word = ngram[1] 
-        new_key = (second_word, choice(chains[ngram])) 
-        text += " {}".format(new_key[1]) 
+        second_word = ngram[1] #ngram end references
+        new_key = (second_word, choice(chains[ngram])) #ngram end reference; creates new key as tuple of two words 
+        text += " {}".format(new_key[1])  #index based on ngram length
         ngram = new_key
         counter +=1
 
@@ -64,10 +68,11 @@ input_path = argv[1]
 input_text = open_and_read_file(input_path)
 
 # # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text,ngram_length)
+print chains
 
 # # # Produce random text
 
-random_text = make_text(chains)
+#random_text = make_text(chains,ngram_length)
 
-print random_text
+#print random_text
